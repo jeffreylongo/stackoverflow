618add778acfd137375556ea80c5a5026906bb47
Jeff Longo <jeffreylongo@yahoo.com>
2017-04-22 14:08:49 -0400

    dear god what is happening...

--- redditTakeTwo/redditTakeTwo/Services/PostServices.cs
+++ redditTakeTwo/redditTakeTwo/Services/PostServices.cs
@@ -1,6 +1,7 @@
 ﻿using redditTakeTwo.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -14,5 +15,18 @@ namespace redditTakeTwo.Services
             var posts = db.Posts.ToList();
             return posts;
         }
+
+        //add post
+        var post = new RedditPost<post>
+            {
+                new Teams{ Image="/Images/dartboard.png", SportId =3,Name="Tampa Dart Dynamos", NumberOfPlayers = 8 },
+                new Teams { Image="/Images/pingpong.jpg", SportId = 6, Name = "St Pete Ping Pong Powerhouse", NumberOfPlayers = 8},
+                new Teams { Image="/Images/golf.jpg", SportId = 5, Name ="Orlando Golf Gang", NumberOfPlayers= 8},
+                new Teams { Image="/Images/dartboard.png", SportId = 5, Name ="Buffalo BullsEyes", NumberOfPlayers= 8},
+                new Teams { Image="/Images/pingpong.jpg", SportId = 5, Name ="Philly Paddlers", NumberOfPlayers= 8},
+                new Teams { Image="/Images/golf.jpg", SportId = 5, Name ="Portland Putters", NumberOfPlayers= 8},
+            };
+        teams.ForEach(team => db.Teams.AddOrUpdate(t => t.Name, team));
+            db.SaveChanges();
     }
 }
