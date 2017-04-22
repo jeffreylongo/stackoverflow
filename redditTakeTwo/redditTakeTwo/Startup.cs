92e17d2737f469e7e15af11c64ca9110d1c246a9
Jeff Longo <jeffreylongo@yahoo.com>
2017-04-22 12:17:21 -0400

    project files...takeTwo actually made a db this time.

---
+++ redditTakeTwo/redditTakeTwo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(redditTakeTwo.Startup))]
+namespace redditTakeTwo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
