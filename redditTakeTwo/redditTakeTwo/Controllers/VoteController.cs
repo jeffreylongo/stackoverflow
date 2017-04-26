5e892b8c85dc8e424679f5a03884d725930dcaf1
Jeff Longo <jeffreylongo@yahoo.com>
2017-04-26 14:07:58 -0400

    added files to try to get updvote downvote to work.

---
+++ redditTakeTwo/redditTakeTwo/Controllers/VoteController.cs
@@ -0,0 +1,34 @@
+﻿using redditTakeTwo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace redditTakeTwo.Controllers
+{
+    public class VoteController : ApiController
+    {
+        // GET: Vote
+        public ActionResult Up(int id)
+        {
+            var db = new ApplicationDbContext();
+            var post = db.Posts.FirstOrDefault(f => f.Id == id);
+            post.UpVotes += 1;
+            db.SaveChanges();
+            return PartialView("_voteDisplay", post);
+        }
+        // GET: Vote
+        public ActionResult Down(int id)
+        {
+            var db = new ApplicationDbContext();
+            var post = db.Posts.FirstOrDefault(f => f.Id == id);
+            post.DownVotes += 1;
+            db.SaveChanges();
+            return PartialView("_voteDisplay", post);
+        }
+    }
+}
+
