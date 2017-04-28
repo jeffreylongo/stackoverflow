865c171a6300df1a0430764dc1d4d3122c161889
Jeff Longo <jeffreylongo@yahoo.com>
2017-04-28 15:01:53 -0400

    admin and user roles set in config file ONLY at this point.

---
+++ redditTakeTwo/redditTakeTwo/Migrations/201704281848187_userRoleDefined.cs
@@ -0,0 +1,16 @@
+namespace redditTakeTwo.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class userRoleDefined : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
--- redditTakeTwo/redditTakeTwo/Migrations/Configuration.cs
+++ redditTakeTwo/redditTakeTwo/Migrations/Configuration.cs
@@ -1,5 +1,8 @@
 namespace redditTakeTwo.Migrations
 {
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using redditTakeTwo.Models;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -15,18 +18,36 @@ namespace redditTakeTwo.Migrations
 
         protected override void Seed(redditTakeTwo.Models.ApplicationDbContext context)
         {
-            //  This method will be called after migrating to the latest version.
+            var userRole = "user";
+            var adminRole = "admin";
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            var store = new RoleStore<IdentityRole>(context);
+            var manager = new RoleManager<IdentityRole>(store);
+
+            if (!context.Roles.Any(a => a.Name == userRole))
+            {
+                var role = new IdentityRole { Name = userRole };
+                manager.Create(role);
+            }
+
+            if (!context.Roles.Any(a => a.Name == adminRole))
+            {
+                var role = new IdentityRole { Name = adminRole };
+                manager.Create(role);
+            }
+
+            var defaultAdmin = "Admin@tiy.com";
+            var password = "Password1!";
+
+            if (!context.Users.Any(a => a.UserName == defaultAdmin))
+            {
+                var userStore = new UserStore<ApplicationUser>(context);
+                var userManager = new UserManager<ApplicationUser>(userStore);
+                var user = new ApplicationUser { UserName = defaultAdmin };
+
+                userManager.Create(user, password);
+                userManager.AddToRole(user.Id, adminRole);
+            }
         }
     }
 }
